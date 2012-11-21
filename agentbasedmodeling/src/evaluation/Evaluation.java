b76e5b7a402f5d733e68d057dba3f3514f942838
eelinei <eelinei+github@gmail.com>
2012-11-21 09:40:11 -0500

    Revert "current commit of project"
    This reverts commit 330450ea43270e4b27bad87c5ffdbb0dc518221a.

--- agentbasedmodeling/src/evaluation/Evaluation.java
+++
@@ -1,7 +0,0 @@
-package evaluation;
-
-import agentmanager.Message;
-
-public interface Evaluation {
-	public Message evaluate(Message message, AgentState state);
-}
--- agentbasedmodeling/src/outputservice/OutputType.java
+++
@@ -1,5 +0,0 @@
-package outputservice;
-
-public enum OutputType {
-	SQL, ERROR, INFO, SQL_ERROR
-}
